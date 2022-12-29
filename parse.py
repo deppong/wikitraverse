758d7bb947a2c51a97d339f5fc574a704b64fc67
deppong <isaiahjdeppong@gmail.com>
2022-12-29 17:36:04 -0500

    added better parsing and output the links to a file with .reduced as the ending. only will process files ending in .html

--- parse.py
+++ parse.py
@@ -1,12 +1,21 @@
 import re
+import os
 
-with open('wiki_test/Eutreta_fenestra.html', 'r') as file:
-    data = file.read()
-    # for some reason using a capture group messed this up, but this is a 
-    # prototype after all
-    out = set(re.findall("\"\/wiki\/C?a?t?e?g?o?r?y?:?\w+\"", str(data)))
+path = os.path.join(os.getcwd(), 'wiki_test')
 
-    for item in out:
-        item = item[7:-1]
-        print(item)
+for filename in os.listdir(path):
+    if filename.endswith('.html'):
+        with open(os.path.join(path, filename), 'r') as file:
+            data = file.read()
+            # for some reason using a capture group messed this up, but this is a 
+            # prototype after all
+            out = set(re.findall("<a href=\"(?:https:\/\/en.wikipedia.org)?\/wiki\/(?:Category:)?\w+\"", str(data)))
+
+            print(filename)
+            with open(os.path.join(path, filename + '.reduced'), 'w') as output:
+                for item in out:
+                    item = re.sub("<a href=\"(?:https:\/\/en.wikipedia.org)?\/wiki\/",'',item)[:-1]
+                
+                    print(f'\t {item}')
+                    output.write(item + '\n')
 
